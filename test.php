4562dfd375020c157db9a071a33815c5e23af72a
RassulAli <rasul.aliyev94@gmail.com>
2019-08-15 17:35:29 +0400

    first commit

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php 
+function changeTheWord($word){
+    $alphabet = 'abcdefghijklmnopqrstuvwzyz';
+    $len = strlen($word);
+    $res = '';
+for($i = 0; $i < $len; ++$i) {
+  $pos = strpos($alphabet, $word[$i]);
+  if ($word[$i] == 'z'){
+    $res[$i] = 'a';
+    }
+  else {
+    $res[$i] = $alphabet[$pos+1];
+    }
+}
+  
+    echo $res;  
+}
+
+changeTheWord('rzkzl');
+
+?>
